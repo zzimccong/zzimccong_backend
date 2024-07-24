493e3c382bde3c6f089fffbe79e3996a92837170
dokwonjae <dobear7@naver.com>
2024-07-24 14:28:51 +0900

    create project

---
+++ src/test/java/com/project/zzimccong/ZzimccongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.zzimccong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzimccongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
