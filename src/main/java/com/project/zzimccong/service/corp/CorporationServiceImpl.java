cbfac586b6d16d3c2fb2407a0d6795ebbabb2391
shin6729 <ggg9905@naver.com>
2024-07-28 00:11:05 +0900

    Feat: "기업 회원 로그인 기능"
    기업 회원 로그인 기능 구현
    
    Resolves: #6

--- src/main/java/com/project/zzimccong/service/corp/CorporationServiceImpl.java
+++ src/main/java/com/project/zzimccong/service/corp/CorporationServiceImpl.java
@@ -3,17 +3,22 @@ package com.project.zzimccong.service.corp;
 import com.project.zzimccong.model.dto.corp.CorporationDTO;
 import com.project.zzimccong.model.entity.corp.Corporation;
 import com.project.zzimccong.repository.corp.CorporationRepository;
+import com.project.zzimccong.security.jwt.JwtTokenUtil;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CorporationServiceImpl implements CorporationService {
 
     private final CorporationRepository corporationRepository;
+    private final JwtTokenUtil jwtTokenUtil;
     private final PasswordEncoder passwordEncoder;
 
-    public CorporationServiceImpl(CorporationRepository corporationRepository, PasswordEncoder passwordEncoder) {
+    public CorporationServiceImpl(CorporationRepository corporationRepository, JwtTokenUtil jwtTokenUtil, PasswordEncoder passwordEncoder) {
         this.corporationRepository = corporationRepository;
+        this.jwtTokenUtil = jwtTokenUtil;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -51,5 +56,22 @@ public class CorporationServiceImpl implements CorporationService {
     @Override
     public boolean isCorpIdExists(String corpId) {
         return corporationRepository.existsByCorpId(corpId);
+    }
+
+    @Override
+    public String authenticate(String corpId, String password) {
+        Optional<Corporation> corporationOptional = corporationRepository.findByCorpId(corpId);
+        if (corporationOptional.isPresent()) {
+            Corporation corporation = corporationOptional.get();
+            if (passwordEncoder.matches(password, corporation.getPassword())) {
+                return jwtTokenUtil.generateToken(corpId, "corp");
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Corporation getCorporationById(String corpId) {
+        return corporationRepository.findByCorpId(corpId).orElseThrow(() -> new IllegalArgumentException("Corporation not found"));
     }
 }
