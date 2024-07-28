91714f1e04855a181ee95f8916c308352de9bee0
신동구 <ggg9905@naver.com>
2024-07-28 15:36:41 +0900

    Feat: "일반 회원 로그인 기능"
    일반 회원 로그인 기능
    
    Resolves: #17

--- src/main/java/com/project/zzimccong/config/SecurityConfig.java
+++ src/main/java/com/project/zzimccong/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.project.zzimccong.config;
 
+import com.project.zzimccong.security.jwt.JwtAuthenticationFilter;
+import com.project.zzimccong.security.jwt.JwtTokenUtil;
+import com.project.zzimccong.security.service.corp.CustomCorpDetailsService;
+import com.project.zzimccong.security.service.user.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,10 +12,12 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,34 +26,52 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    private final JwtTokenUtil jwtTokenUtil;
+    private final CustomCorpDetailsService corpDetailsService;
+    private final CustomUserDetailsService userDetailsService;
+
+    public SecurityConfig(JwtTokenUtil jwtTokenUtil, CustomCorpDetailsService corpDetailsService, CustomUserDetailsService userDetailsService) {
+        this.jwtTokenUtil = jwtTokenUtil;
+        this.corpDetailsService = corpDetailsService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    // CORS 설정
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                // 모든 경로에 대해 http://localhost:3000에서 오는 요청을 허용
                 registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://10.10.10.199:3000").allowedMethods("*");
             }
         };
     }
 
+    // Spring Security 필터 체인을 설정
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+                // HTTP 요청에 대한 보안 설정을 정의
                 .authorizeHttpRequests(auth ->
-                        auth.antMatchers("/api/corporations/corp-register").permitAll()
-                                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                                .anyRequest().authenticated())
-                .csrf().disable()
+                        auth.antMatchers("/**", "/api/auth/**").permitAll() // 모든 경로와 회원가입 및 로그인 요청을 허용
+                                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll() // 모든 OPTIONS 요청을 허용
+                                .anyRequest().authenticated()) // 나머지 모든 요청은 인증 필요함
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 보호를 비활성화
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 관리를 Stateless 방식으로 설정함
+                // JWT 인증 필터를 UsernamePasswordAuthenticationFilter 전에 추가
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenUtil, corpDetailsService, userDetailsService), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
+    // PasswordEncoder를 빈으로 등록
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    // AuthenticationManager를 빈으로 등록
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
---
+++ src/main/java/com/project/zzimccong/model/dto/user/UserDTO.java
@@ -0,0 +1,93 @@
+package com.project.zzimccong.model.dto.user;
+
+public class UserDTO {
+    private String loginId;
+    private String password;
+    private String name;
+    private String birth;
+    private String email;
+    private String phone;
+    private String role;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String loginId, String password, String name, String birth, String email, String phone, String role) {
+        this.loginId = loginId;
+        this.password = password;
+        this.name = name;
+        this.birth = birth;
+        this.email = email;
+        this.phone = phone;
+        this.role = role;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "loginId='" + loginId + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", birth='" + birth + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
