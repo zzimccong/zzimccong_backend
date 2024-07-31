1f05c531c4a6dbd5a8d9e657e192ce0e26ae91be
신동구 <ggg9905@naver.com>
2024-07-31 22:41:59 +0900

    Feat: "기업 회원 비밀번호 찾기 기능"
    기업 회원 비밀번호 찾기 기능 (비밀번호 초기화 후 임시 비밀번호 이메일 전송)
    
    Resolves: #26

--- src/main/java/com/project/zzimccong/service/email/EmailVerificationService.java
+++ src/main/java/com/project/zzimccong/service/email/EmailVerificationService.java
@@ -6,4 +6,5 @@ import com.project.zzimccong.model.dto.email.EmailDTO;
 public interface EmailVerificationService {
     void sendVerificationEmail(EmailDTO emailDTO);
     boolean verifyCode(EmailDTO emailDTO);
+    void sendTemporaryPassword(String corpId, String userId, String email);
 }
--- src/main/java/com/project/zzimccong/service/email/EmailVerificationServiceImpl.java
+++ src/main/java/com/project/zzimccong/service/email/EmailVerificationServiceImpl.java
@@ -1,12 +1,18 @@
 package com.project.zzimccong.service.email;
 
 import com.project.zzimccong.model.dto.email.EmailDTO;
+import com.project.zzimccong.model.entity.corp.Corporation;
+import com.project.zzimccong.model.entity.user.User;
+import com.project.zzimccong.repository.corp.CorporationRepository;
+import com.project.zzimccong.repository.user.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
+import java.util.Optional;
 import java.util.Random;
 
 @Slf4j
@@ -15,10 +21,16 @@ public class EmailVerificationServiceImpl implements EmailVerificationService {
 
     private final JavaMailSender javaMailSender;
     private final TemporaryStorageService temporaryStorageService;
+    private final CorporationRepository corporationRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public EmailVerificationServiceImpl(JavaMailSender javaMailSender, TemporaryStorageService temporaryStorageService) {
+    public EmailVerificationServiceImpl(JavaMailSender javaMailSender, TemporaryStorageService temporaryStorageService, CorporationRepository corporationRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.javaMailSender = javaMailSender;
         this.temporaryStorageService = temporaryStorageService;
+        this.corporationRepository = corporationRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -55,5 +67,72 @@ public class EmailVerificationServiceImpl implements EmailVerificationService {
         log.info("Generated verification code: " + verificationCode);
 
         return String.valueOf(code);
+    }
+
+    @Override
+    public void sendTemporaryPassword(String corpId, String userId, String email) {
+        if (corpId != null) {
+            Optional<Corporation> corporationOptional = corporationRepository.findByCorpId(corpId);
+            if (corporationOptional.isPresent()) {
+                Corporation corporation = corporationOptional.get();
+                if (corporation.getCorpEmail().equals(email)) {
+                    String tempPassword = generateTemporaryPassword();
+                    corporation.setPassword(passwordEncoder.encode(tempPassword));
+                    corporationRepository.save(corporation);
+
+                    SimpleMailMessage mailMessage = new SimpleMailMessage();
+                    mailMessage.setTo(email);
+                    mailMessage.setSubject("찜꽁 테이블 임시 비밀번호");
+                    mailMessage.setText("임시 비밀번호 : " + tempPassword + "\n임시 비밀번호는 즉시 변경을 권장 드립니다.");
+                    mailMessage.setFrom("ggg9905@naver.com");
+
+                    try {
+                        javaMailSender.send(mailMessage);
+                    } catch (Exception e) {
+                        System.out.println("이메일 발송 오류 : " + e.getMessage());
+                        e.printStackTrace();
+                    }
+                } else {
+                    throw new IllegalArgumentException("Email does not match");
+                }
+            } else {
+                throw new IllegalArgumentException("Corporation not found");
+            }
+        }
+
+        if (userId != null) {
+            Optional<User> userOptional = userRepository.findByLoginId(userId);
+            if (userOptional.isPresent()) {
+                User user = userOptional.get();
+                if (user.getEmail().equals(email)) {
+                    String tempPassword = generateTemporaryPassword();
+                    user.setPassword(passwordEncoder.encode(tempPassword));
+                    userRepository.save(user);
+
+                    SimpleMailMessage mailMessage = new SimpleMailMessage();
+                    mailMessage.setTo(email);
+                    mailMessage.setSubject("찜꽁 테이블 임시 비밀번호");
+                    mailMessage.setText("임시 비밀번호 : " + tempPassword + "\n임시 비밀번호는 즉시 변경을 권장 드립니다.");
+                    mailMessage.setFrom("ggg9905@naver.com");
+
+                    try {
+                        javaMailSender.send(mailMessage);
+                    } catch (Exception e) {
+                        System.out.println("이메일 발송 오류 : " + e.getMessage());
+                        e.printStackTrace();
+                    }
+                } else {
+                    throw new IllegalArgumentException("Email does not match");
+                }
+            } else {
+                throw new IllegalArgumentException("User not found");
+            }
+        }
+    }
+
+    private String generateTemporaryPassword() {
+        Random random = new Random();
+        int password = 100000 + random.nextInt(900000);
+        return String.valueOf(password);
     }
 }
