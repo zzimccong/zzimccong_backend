b4c986e7da550ffcedd7b7ea74ee025dfd268d3a
신동구 <ggg9905@naver.com>
2024-08-05 02:36:10 +0900

    Feat: "관리자 기능 유저 상세 조회" (#51)
    * 기업 회원가입 기능 구현
    
    * Feat: "기업 회원 로그인 기능"
    
    기업 회원 로그인 기능 구현
    
    Resolves: #6
    
    * Feat: "관리자 기능 유저 상세 조회"
    
    관리자 기능 유저 상세 조회
    
    Resolves: #47

--- src/main/java/com/project/zzimccong/controller/admin/AdminController.java
+++ src/main/java/com/project/zzimccong/controller/admin/AdminController.java
@@ -4,8 +4,10 @@ import com.project.zzimccong.model.entity.corp.Corporation;
 import com.project.zzimccong.model.entity.user.User;
 import com.project.zzimccong.service.corp.CorporationService;
 import com.project.zzimccong.service.user.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +37,21 @@ public class AdminController {
     public ResponseEntity<List<Corporation>> getAllCorps() {
         List<Corporation> corp = corporationService.getAllCorps();
         return ResponseEntity.ok(corp); // 200 OK 상태와 함께 사용자 목록 반환
+    }
+
+    // 일반 사용자 상세 정보 조회 엔드포인트 (관리자용)
+    @GetMapping("/edit-user/{id}")
+    public ResponseEntity<User> getUserByIdForAdmin(@PathVariable Integer id) {
+        try {
+            User user = userService.getFindById(id);
+            if (user != null) {
+                return ResponseEntity.ok(user);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
 
