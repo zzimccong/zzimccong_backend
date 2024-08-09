67375d99906b1d725e9e26fff61fd2ac089ca74a
dudy9436 <dudy9436@gmail.com>
2024-08-09 13:17:11 +0900

    Feat: "일반 회원 할인권 조회"

--- src/main/java/com/project/zzimccong/service/coupon/CouponService.java
+++ src/main/java/com/project/zzimccong/service/coupon/CouponService.java
@@ -1,5 +1,8 @@
 package com.project.zzimccong.service.coupon;
 
+import com.project.zzimccong.model.entity.coupon.Coupon;
+
 public interface CouponService {
     Integer findCntByUserIdAndType(Integer userId, String type);
+    Coupon findDiscountCouponByUserId(Integer userId);
 }
