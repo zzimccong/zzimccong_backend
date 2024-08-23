0a81fca92c70b70060ba6b7f550b7d6591f9c252
신동구 <ggg9905@naver.com>
2024-08-23 10:36:08 +0900

    Feat: "알림 발송 로직 구현" (#126)
    * 기업 회원가입 기능 구현
    
    * Feat: "기업 회원 로그인 기능"
    
    기업 회원 로그인 기능 구현
    
    Resolves: #6
    
    * Feat: "알림 발송 로직 구현"
    
    알림 발송 로직 구현 (기업, 유저)
    
    Resolves: #123
    
    * Feat: "알림 발송 로직 구현"
    
    알림 발송 로직 구현 (기업, 유저)
    json 파일 제거
    
    Resolves: #123

--- src/main/java/com/project/zzimccong/service/notification/NotificationService.java
+++ src/main/java/com/project/zzimccong/service/notification/NotificationService.java
@@ -1,5 +1,7 @@
 package com.project.zzimccong.service.notification;
 
+import java.util.List;
+
 public interface NotificationService {
     void saveUserToken(Integer userId, String token);
     void saveCorpToken(Integer corpId, String token);
@@ -10,6 +12,9 @@ public interface NotificationService {
 
     void deleteUserToken(Integer userId);  // 사용자 토큰 삭제 메서드
     void deleteCorpToken(Integer corpId);  // 기업 토큰 삭제 메서드
+
+    void sendMessage(String token, String title, String message) throws Exception;
+    void sendMessages(List<String> tokens, String title, String message) throws Exception;
 
 
 }
