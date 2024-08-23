94c70baed13e8a29e48b7e74220f515569ccc65a
신동구 <ggg9905@naver.com>
2024-08-23 09:58:22 +0900

    Feat: "FCM 토큰 삭제 기능 구현" (#125)
    * 기업 회원가입 기능 구현
    
    * Feat: "기업 회원 로그인 기능"
    
    기업 회원 로그인 기능 구현
    
    Resolves: #6
    
    * Feat: "FCM 토큰 삭제 기능 구현"
    
    FCM 토큰 삭제 기능 구현
    
    Resolves: #122

--- src/main/java/com/project/zzimccong/service/notification/NotificationService.java
+++ src/main/java/com/project/zzimccong/service/notification/NotificationService.java
@@ -8,4 +8,8 @@ public interface NotificationService {
 
     String getCorpToken(Integer corpId);
 
+    void deleteUserToken(Integer userId);  // 사용자 토큰 삭제 메서드
+    void deleteCorpToken(Integer corpId);  // 기업 토큰 삭제 메서드
+
+
 }
