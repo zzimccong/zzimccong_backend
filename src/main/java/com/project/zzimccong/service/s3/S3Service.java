97d6a1c52f46ed768081c419fa872b690d2cfc65
dokwonjae <141594810+dokwonjae@users.noreply.github.com>
2024-08-26 00:00:25 +0900

    Feat: "AWS s3 기능 구현" (#142)
    * aws s3 추가
    
    * aws s3 추가

---
+++ src/main/java/com/project/zzimccong/service/s3/S3Service.java
@@ -0,0 +1,15 @@
+package com.project.zzimccong.service.s3;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface S3Service {
+
+    // 레스토랑 사진 업로드
+    String uploadRestaurantPhoto(MultipartFile file, Long restaurantId);
+
+    // 레스토랑 사진 삭제
+    void deleteRestaurantPhoto(String photoUrl, Long restaurantId);
+
+    // 레스토랑 사진 URL 조회
+    String getRestaurantPhotoUrl(Long restaurantId);
+}
