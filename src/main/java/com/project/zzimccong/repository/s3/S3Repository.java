0d23bae30faa7447e8d7e3d1c3df2fbf1f30e80a
dokwonjae <141594810+dokwonjae@users.noreply.github.com>
2024-08-27 10:51:01 +0900

    배포 테스트 (#174)

--- src/main/java/com/project/zzimccong/repository/s3/S3Repository.java
+++ src/main/java/com/project/zzimccong/repository/s3/S3Repository.java
@@ -1,47 +1,47 @@
-package com.project.zzimccong.repository.s3;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Repository;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
-
-@Repository
-public class S3Repository {
-
-    @Value("${aws.s3.bucketName}")
-    private String bucketName;
-
-    private final S3Client s3Client;
-
-    public S3Repository(S3Client s3Client) {
-        this.s3Client = s3Client;
-    }
-
-    public String uploadFileToS3(String fileName, String filePath) {
-        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                .bucket(bucketName)
-                .key(fileName)
-                .build();
-
-        PutObjectResponse response = s3Client.putObject(putObjectRequest, software.amazon.awssdk.core.sync.RequestBody.fromFile(new java.io.File(filePath)));
-        return getFileUrl(fileName);
-    }
-
-    public String getFileUrl(String fileName) {
-        return s3Client.utilities().getUrl(builder -> builder.bucket(bucketName).key(fileName)).toExternalForm();
-    }
-
-    public void deleteFileFromS3(String fileName) {
-        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
-                .bucket(bucketName)
-                .key(fileName)
-                .build();
-
-        s3Client.deleteObject(deleteObjectRequest);
-    }
-
-
-
-}
+//package com.project.zzimccong.repository.s3;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Repository;
+//import software.amazon.awssdk.services.s3.S3Client;
+//import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+//import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+//import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+//
+//@Repository
+//public class S3Repository {
+//
+//    @Value("${aws.s3.bucketName}")
+//    private String bucketName;
+//
+//    private final S3Client s3Client;
+//
+//    public S3Repository(S3Client s3Client) {
+//        this.s3Client = s3Client;
+//    }
+//
+//    public String uploadFileToS3(String fileName, String filePath) {
+//        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+//                .bucket(bucketName)
+//                .key(fileName)
+//                .build();
+//
+//        PutObjectResponse response = s3Client.putObject(putObjectRequest, software.amazon.awssdk.core.sync.RequestBody.fromFile(new java.io.File(filePath)));
+//        return getFileUrl(fileName);
+//    }
+//
+//    public String getFileUrl(String fileName) {
+//        return s3Client.utilities().getUrl(builder -> builder.bucket(bucketName).key(fileName)).toExternalForm();
+//    }
+//
+//    public void deleteFileFromS3(String fileName) {
+//        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+//                .bucket(bucketName)
+//                .key(fileName)
+//                .build();
+//
+//        s3Client.deleteObject(deleteObjectRequest);
+//    }
+//
+//
+//
+//}
