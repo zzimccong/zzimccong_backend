4e7c67ff331c099d593591e2db9fb155f51c3941
dokwonjae <dobear7@naver.com>
2024-08-27 16:30:30 +0900

    aws 코드 변경

--- src/main/java/com/project/zzimccong/config/S3Config.java
+++ src/main/java/com/project/zzimccong/config/S3Config.java
@@ -1,31 +1,19 @@
 package com.project.zzimccong.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
 
-    @Value("${aws.accessKeyId}")
-    private String accessKey;
-
-    @Value("${aws.secretAccessKey}")
-    private String secretKey;
-
-    @Value("${aws.region}")
-    private String region;
-
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .region(Region.of("ap-northeast-3")) // region을 직접 설정하거나 properties에서 불러올 수 있습니다.
+                .credentialsProvider(InstanceProfileCredentialsProvider.create())
                 .build();
     }
 }
